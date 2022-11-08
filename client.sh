dadd034b3f5f1a7c7d6f3c68717fdeddf0d19453
Rafa Laguna <rafa.laguna@enti.cat>
2022-11-08 11:26:34 +0100

    Sending data

--- client.sh
+++ client.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
+IP_SERVER="localhost"
+IP_LOCAL="127.0.0.1"
+
+PORT="4242"
+
 echo "Cliente HMTP"
 
 echo "(1) SEND - Enviando el Handshake"
 
-echo "GREEN_POWA 127.0.0.1" | nc localhost 4242
+echo "GREEN_POWA $IP_LOCAL" | nc $IP_SERVER $PORT
 
 echo "(2) LISTEN - Escuchando confirmación"
 
-MSG=`nc -l 4242`
+MSG=`nc -l $PORT`
 
 if [ "$MSG" != "OK_HMTP" ]
 then
@@ -16,4 +21,41 @@ then
 	exit 1
 fi
 
-echo "seguimos"
+echo "(5) SEND - Enviamos el nombre de archivo"
+
+FILE_NAME="elon_musk.jpg"
+
+FILE_MD5=`echo $FILE_NAME | md5sum | cut -d " " -f 1`
+
+echo "FILE_NAME $FILE_NAME $FILE_MD5" | nc $IP_SERVER $PORT
+
+echo "(6) LISTEN - Escuchando confirmación nombre archivo"
+
+MSG=`nc -l $PORT`
+
+if [ "$MSG" != "OK_FILE_NAME" ]
+then
+	echo "ERROR 2: Nombre de archivo enviado incorrectamente"
+	echo "	Mensaje de error: $MSG"
+
+	exit 2
+fi
+
+echo "(9) SEND - Enviamos datos del archivo"
+
+cat memes/$FILE_NAME | nc $IP_SERVER $PORT
+
+echo "(10) LISTEN - Escuchamos confirmación datos archivo"
+
+MSG=`nc -l $PORT`
+
+if [ "$MSG" != "OK_DATA_RCPT" ]
+then
+	echo "ERROR 3: Datos enviados incorrectamente"
+	exit 3
+fi
+
+
+echo "Fin del envío"
+
+exit 0
--- server.sh
+++ server.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+PORT="4242"
+
 echo "Servidor HMTP"
 
 echo "(0) LISTEN - Levantando el servidor"
 
-MSG=`nc -l 4242`
+MSG=`nc -l $PORT`
 
 HANDSHAKE=`echo $MSG | cut -d " " -f 1`
 IP_CLIENT=`echo $MSG | cut -d " " -f 2`
@@ -13,15 +15,50 @@ echo "(3) SEND - Confirmación Handshake"
 
 if [ "$HANDSHAKE" != "GREEN_POWA" ]
 then
-	echo "KO_HMTP" | nc $IP_CLIENT 4242
+	echo "KO_HMTP" | nc $IP_CLIENT $PORT
 	exit 1
 fi
 
-echo "OK_HMTP" | nc $IP_CLIENT 4242
+echo "OK_HMTP" | nc $IP_CLIENT $PORT
 
-echo "(4) LISTEN"
+echo "(4) LISTEN - Escuchando el nombre de archivo"
 
-MSG=`nc -l 4242`
+MSG=`nc -l $PORT`
 
+PREFIX=`echo $MSG | cut -d " " -f 1`
+FILE_NAME=`echo $MSG | cut -d " " -f 2`
+FILE_MD5=`echo $MSG | cut -d " " -f 3`
+
+echo "(7) SEND - Confirmación nombre de archivo"
+
+if [ "$PREFIX" != "FILE_NAME" ]
+then
+	echo "KO_FILE_NAME" | nc $IP_CLIENT $PORT
+	exit 2
+fi
+
+MD5SUM=`echo $FILE_NAME | md5sum | cut -d " " -f 1`
+
+if [ "$MD5SUM" != "$FILE_MD5" ]
+then
+	echo "KO_FILE_MD5" | nc $IP_CLIENT $PORT
+	exit 3
+fi
+
+
+
+
+echo "OK_FILE_NAME" | nc $IP_CLIENT $PORT
+
+echo "(8) LISTEN - Escuchando datos de archivo"
+
+nc -l $PORT > inbox/$FILE_NAME
+
+echo "(11) SEND - Confirmación recepción datos"
+
+echo "OK_DATA_RCPT" | nc $IP_CLIENT $PORT
+
+
+echo "Fin de la recepción"
 
 exit 0
